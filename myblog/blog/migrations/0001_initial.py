8d1566691fa353a2b4ae149cf5d8f6d1fcb82a40
bigharshrag <garg.rishabh22@gmail.com>
2015-10-23 15:55:41 +0530

    First Try

---
+++ myblog/blog/migrations/0001_initial.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Post',
+            fields=[
+                ('id', models.AutoField(serialize=False, primary_key=True, verbose_name='ID', auto_created=True)),
+                ('title', models.CharField(max_length=200)),
+                ('text', models.TextField()),
+                ('created_date', models.DateTimeField(auto_now_add=True)),
+            ],
+        ),
+    ]
