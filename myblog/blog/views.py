a2c9a40423a2e57857484b6f0c3aaad56fe9a4df
bigharshrag <garg.rishabh22@gmail.com>
2015-10-23 19:55:54 +0530

    Added ability to display and add blogs

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
 from .models import Post
+from .forms import PostForm
+from django.shortcuts import redirect
+
 
 # Create your views here.
 def post_list(request):
-	posts=Post.objects.order_by('created_date')
+	posts=Post.objects.order_by('-created_date')
 	return render(request, 'blog/post_list.html', {'posts':posts})
+
+def post_new(request):
+	if request.method == "POST":
+		form = PostForm(request.POST)
+		if form.is_valid():
+			post = form.save()
+			post.save()
+			return redirect('blog.views.post_list')
+	else:
+		form = PostForm()
+	return render(request, 'blog/post_edit.html', {'form': form})
